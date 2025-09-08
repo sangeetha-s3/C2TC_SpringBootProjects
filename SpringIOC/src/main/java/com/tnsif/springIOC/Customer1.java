5c0baa0989c89c7047236b1503c43d4ba8a853dd
sange <sange@hp>
2025-09-08 20:31:04 +0530

    Commit

---
+++ SpringIOC/src/main/java/com/tnsif/springIOC/Customer1.java
@@ -0,0 +1,36 @@
+package com.tnsif.springIOC;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer1 {
+	 public Customer1()
+	 {
+		 System.out.println("Customer1 class object has been created");
+	 }
+	 public void order()
+	 {
+		 System.out.println("Customer1 ordering some food");
+	 }
+     public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	 private int cid;
+     private String name;
+     private String city;
+}
---
+++ SpringIOC/src/main/java/com/tnsif/springIOC/SpringIocApplication.java
@@ -0,0 +1,18 @@
+package com.tnsif.springIOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringIocApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext var = SpringApplication.run(SpringIocApplication.class, args);
+		Shop s1 = var.getBean(Shop.class);
+		s1.ordering();
+		Customer1 c1 = var.getBean(Customer1.class);
+		c1.order();
+	}
+
+}
